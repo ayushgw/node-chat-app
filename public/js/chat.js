5d45d1e9162a5edfdf7d8416b51069d487cca72e
ayushgw <ayushgoswami@zoho.com>
2019-08-13 02:04:28 +0530

    form & button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,44 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (msg) => {
     console.log(msg);
 })
 
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const msg = event.target.elements.message.value
     socket.emit('sendMessage', msg, (error) => {
-        if(error) {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
             return console.error(error);
         }
         console.log('Message delivered!');
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
-    if(!navigator.geolocation) {
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser!')
     }
 
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((location) => {
         socket.emit('sendLocation', {
             latitude: location.coords.latitude,
             longitude: location.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location Shared!');
         })
     })
