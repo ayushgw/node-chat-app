520586fa520bd32bcc62d3422a4a940316e33a06
ayushgw <ayushgoswami@zoho.com>
2019-08-13 23:19:43 +0530

    storing & deleting users

---
+++ src/utils/users.js
@@ -0,0 +1,38 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({id, username, room}) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if(!username || !room) {
+        return {
+            error: 'Username and Room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find(user => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if(existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = id => {
+    const index = users.findIndex(user => user.id === id)
+    return users.splice(index, 1)[0]
+}
